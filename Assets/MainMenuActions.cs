bb90f8e6d455bd73a3402a4333e9e77f9fbbb116
Jorge Mora <jorgemg354@gmail.com>
2023-11-26 18:12:10 -0800

    2.2 y 2.3

--- Assets/MainMenuActions.cs
+++ Assets/MainMenuActions.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuActions : MonoBehaviour
 {
+    public void GoToScene (string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp() {
+        Application.Quit();
+        Debug.Log("Quitting game");
+    }
+
     public void RedirectToURL(string url) {
         Application.OpenURL(url);
     }
